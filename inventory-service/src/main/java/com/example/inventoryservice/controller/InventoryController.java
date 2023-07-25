2866944dbf14195c301c6d9f55b8eb7cd41b71ae
PetkoLyuts <simona2000petko@abv.bg>
2023-07-25 20:42:39 +0300

    Implement addProductInInventory functionality for the inventory-service

--- inventory-service/src/main/java/com/example/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/example/inventoryservice/controller/InventoryController.java
@@ -1,5 +1,6 @@
 package com.example.inventoryservice.controller;
 
+import com.example.inventoryservice.dto.InventoryRequestDto;
 import com.example.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,12 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class InventoryController {
     private final InventoryService inventoryService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addProductInInventory(@RequestBody InventoryRequestDto inventoryRequestDto) {
+        inventoryService.addProductInInventory(inventoryRequestDto);
+    }
 
     @GetMapping("/{skuCode}")
     @ResponseStatus(HttpStatus.OK)
--- inventory-service/src/main/java/com/example/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/example/inventoryservice/service/InventoryService.java
@@ -1,11 +1,15 @@
 package com.example.inventoryservice.service;
 
+import com.example.inventoryservice.dto.InventoryRequestDto;
+import com.example.inventoryservice.model.Inventory;
 import com.example.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
@@ -13,5 +17,18 @@ public class InventoryService {
     @Transactional(readOnly = true)
     public boolean isInStock(String skuCode) {
         return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    }
+
+    public void addProductInInventory(InventoryRequestDto inventoryRequestDto) {
+        Inventory inventory = inventoryRepository.save(mapToInventory(inventoryRequestDto));
+
+        log.info("Inventory has been populated, id = {}", inventory.getId());
+    }
+
+    private Inventory mapToInventory(InventoryRequestDto inventoryRequestDto) {
+        return Inventory.builder()
+               .skuCode(inventoryRequestDto.getSkuCode())
+               .quantity(inventoryRequestDto.getQuantity())
+               .build();
     }
 }
